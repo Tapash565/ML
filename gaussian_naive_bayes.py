3c53bcf1daa911f87ed291bbb2af7a738fa60762
Tapash <kumar.tapash565@gmail.com>
2025-02-11 18:47:41 +0530

    Gaussian Naive Bayes Optimization

--- gaussian_naive_bayes.py
+++ gaussian_naive_bayes.py
@@ -10,16 +10,20 @@ class GaussianNB():
         """
         self.X, self.y = X, y
         self.classes = np.unique(y)
-        self.parameters = []
+        self.parameters = {}
+        eps = 1e-4
         
         for c in self.classes:
             # Extract all rows for a specific class
             X_c = X[y == c]
             # Store mean and variance for each feature
-            self.parameters.append({
+            self.parameters[c] = {
                 "mean": X_c.mean(axis=0),
-                "var": X_c.var(axis=0)
-            })
+                "var": X_c.var(axis=0, ddof=1)  # Use unbiased variance
+            }
+        self.class_counts = np.array([np.sum(y == c) for c in self.classes])
+        self.class_priors = self.class_counts / len(y)
+        self.log_priors = np.log(np.clip(self.class_priors, eps, 1.0))
 
     def _calculate_log_likelihood(self, mean, var, x):
         """
@@ -31,26 +35,19 @@ class GaussianNB():
         coeff = -0.5 * np.log(2.0 * np.pi * var)
         exponent = -0.5 * ((x - mean) ** 2) / var
         return coeff + exponent  # Return log-likelihood
-
-    def _calculate_log_prior(self, c):
-        """
-        Compute the log prior probability for class c.
-        """
-        return np.log(np.mean(self.y == c))
-
-    
+ 
     def _classify(self, X):
         """
         Classify multiple samples by computing the posterior probabilities for each class.
         """
-        log_priors = np.array([self._calculate_log_prior(c) for c in self.classes])
         log_likelihoods = np.zeros((X.shape[0], len(self.classes)))
 
-        for i, params in enumerate(self.parameters):
+        for i, params in self.parameters.items():
             log_likelihoods[:, i] = np.sum(self._calculate_log_likelihood(params["mean"], params["var"], X), axis=1)
 
-        log_posteriors = log_priors + log_likelihoods
+        log_posteriors = self.log_priors + log_likelihoods
         return self.classes[np.argmax(log_posteriors, axis=1)]
+
 
     def predict(self, X):
         """
@@ -73,3 +70,5 @@ class GaussianNB():
         print(f"F1 score: {f1}")
         print(f"Recall: {recall}")
         print(f"Precision: {precision}")
+
+        
